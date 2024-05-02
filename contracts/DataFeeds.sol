f069e0e9bc4c40e3b03af5723b8b84bfb3a3304a
Raad05 <ashabulraad@gmail.com>
2024-05-02 17:00:42 +0600

    solidity eslint config

--- contracts/DataFeeds.sol
+++ contracts/DataFeeds.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
@@ -9,12 +9,14 @@ contract DataFeeds {
 
     // constructor
     constructor() {
-        dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        dataFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
     }
 
     // get latest rounded value
     function getLatestAnswer() public view returns (int) {
-        (, int answer, , ,) = dataFeed.latestRoundData();
+        (, int answer, , , ) = dataFeed.latestRoundData();
         return answer;
-    }    
+    }
 }
